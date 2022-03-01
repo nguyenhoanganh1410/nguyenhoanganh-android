689d367039402c8d82298a7726e477ad0bb1cbd3
hoanganh <hoanganh1410tb@gmail.com>
2022-03-01 18:16:30 +0700

    lab03-screen01-screen03_a

---
+++ app/src/main/java/com/example/myapp/main.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
+public class main extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activyty_lab03_screen03_1);
+    }
+
+//    public void handleOnClick(View v){
+//        Toast.makeText(main.this, "message", Toast.LENGTH_LONG).show();
+//    }
+
+}
